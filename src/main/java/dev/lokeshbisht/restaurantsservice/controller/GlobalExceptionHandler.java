66c3d95ab37354e092cc32b4cb8ce764a4562223
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-06-24 22:30:00 +0530

    Add global exception handler

---
+++ src/main/java/dev/lokeshbisht/restaurantsservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package dev.lokeshbisht.restaurantsservice.controller;
+
+import dev.lokeshbisht.restaurantsservice.dto.ErrorResponseDto;
+import dev.lokeshbisht.restaurantsservice.enums.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.UUID;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleAllException(Exception ex) {
+        UUID uuid = UUID.randomUUID();
+        String message = String.format("Unhandled exception, logged against error id: %s", uuid);
+        log.error("Exception: {} {}", message, ex.getClass().getName(), ex);
+        log.error(ex.getMessage(), ex);
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR, message);
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}
