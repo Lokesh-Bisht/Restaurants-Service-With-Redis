07968f3c5f332ef85b32c2b57a566e54443c437b
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-06-28 22:24:43 +0530

    Add validations for address and restaurant request dto

--- src/main/java/dev/lokeshbisht/restaurantsservice/dto/restaurants/RestaurantDto.java
+++ src/main/java/dev/lokeshbisht/restaurantsservice/dto/restaurants/RestaurantDto.java
@@ -1,6 +1,9 @@
 package dev.lokeshbisht.restaurantsservice.dto.restaurants;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,21 +16,25 @@ public class RestaurantDto {
     @JsonProperty("name")
     private String name;
 
+    @NotNull(message = "RestaurantId is required")
     @JsonProperty("id")
     private Integer restaurantId;
 
     @JsonProperty("uuid")
     private UUID uuid;
 
+    @NotNull(message = "veg flag is required")
     @JsonProperty("veg")
     private Boolean veg;
 
     @JsonProperty("is_new")
     private Boolean isNew;
 
+    @PositiveOrZero(message = "Invalid avg rating. Avg rating can't be negative.")
     @JsonProperty("avg_rating")
     private Float avgRating;
 
+    @PositiveOrZero(message = "Invalid total ratings. Total ratings can't be negative.")
     @JsonProperty("total_ratings")
     private Integer totalRatings;
 
@@ -37,18 +44,22 @@ public class RestaurantDto {
     @JsonProperty("cuisines")
     private List<String> cuisines;
 
+    @Positive(message = "Cost for two should be greater than zero.")
     @JsonProperty("cost_for_two")
     private Integer costForTwo;
 
     @JsonProperty("cost_for_two_string")
     private String costForTwoString;
 
+    @Positive(message = "Delivery time should be greater than zero.")
     @JsonProperty("delivery_time")
     private Double deliveryTime;
 
+    @Positive(message = "Minimum delivery time should be greater than zero.")
     @JsonProperty("min_delivery_time")
     private Double minDeliveryTime;
 
+    @Positive(message = "Maximum delivery time should be greater than zero.")
     @JsonProperty("max_delivery_time")
     private Double maxDeliveryTime;
 
