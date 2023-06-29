40bef762b4bcfdd74e600f16e90aa0bfb456f9b1
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-06-29 20:58:13 +0530

    feat: #12 Add RestaurantUpdateRequest dto To Restaurant document mapper and method to fetch restaurant from restaurants collection

--- src/main/java/dev/lokeshbisht/restaurantsservice/mapper/RestaurantMapper.java
+++ src/main/java/dev/lokeshbisht/restaurantsservice/mapper/RestaurantMapper.java
@@ -1,6 +1,7 @@
 package dev.lokeshbisht.restaurantsservice.mapper;
 
 import dev.lokeshbisht.restaurantsservice.dto.restaurants.RestaurantDto;
+import dev.lokeshbisht.restaurantsservice.dto.restaurants.RestaurantUpdateRequestDto;
 import dev.lokeshbisht.restaurantsservice.model.Restaurant;
 import org.mapstruct.Mapper;
 
@@ -10,4 +11,7 @@ public interface RestaurantMapper {
     Restaurant restaurantDtoToRestaurantMapper(RestaurantDto restaurantDto);
 
     RestaurantDto restaurantToRestaurantDtoMapper(Restaurant restaurant);
+
+    Restaurant restaurantUpdateRequestDtoToRestaurantMapper(RestaurantUpdateRequestDto restaurantUpdateRequestDto);
+
 }
