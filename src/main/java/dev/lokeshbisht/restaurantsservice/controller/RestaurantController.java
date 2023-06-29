7a3b4f95ed8bfac4ac66195e56b80f5a5137129d
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-06-29 20:59:04 +0530

    feat: #12 Add update restaurant info api

--- src/main/java/dev/lokeshbisht/restaurantsservice/controller/RestaurantController.java
+++ src/main/java/dev/lokeshbisht/restaurantsservice/controller/RestaurantController.java
@@ -1,8 +1,9 @@
 package dev.lokeshbisht.restaurantsservice.controller;
 
 import dev.lokeshbisht.restaurantsservice.dto.restaurants.RestaurantDto;
-import dev.lokeshbisht.restaurantsservice.model.Restaurant;
+import dev.lokeshbisht.restaurantsservice.dto.restaurants.RestaurantUpdateRequestDto;
 import dev.lokeshbisht.restaurantsservice.service.RestaurantService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,12 @@ public class RestaurantController {
     private RestaurantService restaurantService;
 
     @PostMapping("/restaurant")
-    public RestaurantDto createRestaurant(@RequestBody RestaurantDto restaurantDto) {
+    public RestaurantDto createRestaurant(@Valid @RequestBody RestaurantDto restaurantDto) {
         return restaurantService.createRestaurant(restaurantDto);
+    }
+
+    @PatchMapping("/restaurant/{restaurantId}")
+    public RestaurantDto updateRestaurant(@Valid @RequestBody RestaurantUpdateRequestDto restaurantUpdateRequestDto, @PathVariable Integer restaurantId) {
+        return restaurantService.updateRestaurant(restaurantUpdateRequestDto, restaurantId);
     }
 }
